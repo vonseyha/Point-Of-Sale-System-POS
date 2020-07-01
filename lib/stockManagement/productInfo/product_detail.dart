1d949101bf94ac2c570d96844ecea064d48c1c68
vonseyha <seyhavon22@gmail.com>
2020-07-01 22:49:00 +0700

    create employee detail page

---
+++ lib/stockManagement/productInfo/product_detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProductDetail extends StatefulWidget{
+  ProductDetail() : super();
+  _ProductDetailScreen createState() => _ProductDetailScreen();
+}
+
+class _ProductDetailScreen extends State<ProductDetail>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
